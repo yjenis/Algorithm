338a1ffdec3477978cc7e2616df40eb8073389d3
YEJIN <147367459+yjenis@users.noreply.github.com>
2024-08-29 11:35:18 +0900

    [level 3] Title: 등대, Time: 201.28 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.py
+++ 프로그래머스/3/133500. 등대/등대.py
@@ -1,31 +1,35 @@
-import sys
-sys.setrecursionlimit(100000)
+from collections import defaultdict
 
 def solution(n, lighthouse):
-    dp = [[0]*2 for _ in range(n)]
-    node_dict = { key : [] for key in range(n) }
-    visited = [False]*n
-    for a, b in lighthouse :
-        node_dict[a-1].append(b-1)
-        node_dict[b-1].append(a-1)
-        
-    def dfs(node) :
-        visited[node] = True
-        leaf_list = list()
-        for leaf in node_dict[node] :
-            if visited[leaf] :
-                continue
-            leaf_list.append(leaf)
-            dfs(leaf)
-
-        dp[node][0] = 0
-        dp[node][1] = 1
-
-        for leaf in leaf_list :
-            dp[node][1] += min(dp[leaf])
-            dp[node][0] += dp[leaf][1]
-        return
+    # 트리의 인접 리스트 표현
+    tree = defaultdict(list)
+    for u, v in lighthouse:
+        tree[u].append(v)
+        tree[v].append(u)
     
-    dfs(0)
+    # DP 테이블 초기화
+    dp = [[0, 0] for _ in range(n + 1)]
+    visited = [False] * (n + 1)
+    
+    def dfs(node):
+        visited[node] = True
+        dp[node][1] = 1  # 노드 켜는 경우 초기값은 1
         
-    return min(dp[0])
+        for neighbor in tree[node]:
+            if not visited[neighbor]:
+                dfs(neighbor)
+                dp[node][0] += dp[neighbor][1]
+                dp[node][1] += min(dp[neighbor][0], dp[neighbor][1])
+    
+    # 1번 노드를 루트로 DFS 시작
+    dfs(1)
+    
+    # 루트 노드의 dp 값 중 최소값이 답
+    return min(dp[1][0], dp[1][1])
+
+# 예시 실행
+n = 8
+lighthouse = [(1,2), (1,3), (1,4), (1,5), (5,6), (5,7), (5,8)]
+print(solution(n, lighthouse))  # 출력: 2
+
+    
