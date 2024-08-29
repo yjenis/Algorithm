b7348b2573049741db4190a9f68d2539f93f7d37
YEJIN <147367459+yjenis@users.noreply.github.com>
2024-08-29 13:01:10 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 412.42 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,22 +1,18 @@
-def solution(sequence, k):
+def solution(sequence, g):
     n = len(sequence)
-    left = 0
-    current_sum = 0
     best_range = None
 
-    for right in range(n):
-        current_sum += sequence[right]
+    for start in range(n):
+        current_sum = 0
+        for end in range(start, n):
+            current_sum += sequence[end]
+            if current_sum == g:
+                # 현재 부분 수열의 길이
+                length = end - start
+                if best_range is None or length < best_range[1] - best_range[0] or (length == best_range[1] - best_range[0] and start < best_range[0]):
+                    best_range = [start, end]
+                break
+            elif current_sum > g:
+                break
 
-        # current_sum이 k를 넘을 때까지 왼쪽 포인터 이동
-        while current_sum > k and left <= right:
-            current_sum -= sequence[left]
-            left += 1
-        
-        # current_sum이 k와 같을 때, 부분 수열의 길이를 확인
-        if current_sum == k:
-            if best_range is None or (right - left < best_range[1] - best_range[0]):
-                best_range = [left, right]
-            elif right - left == best_range[1] - best_range[0] and left < best_range[0]:
-                best_range = [left, right]
-
-    return best_range 
+    return best_range if best_range is not None else []
