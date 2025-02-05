1f35f90301e966b5b6a4f94e014f6d37a841836d
YEJIN <147367459+yjenis@users.noreply.github.com>
2025-02-05 21:11:34 +0900

    [level 2] Title: 주식가격, Time: 147.08 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,12 +1,17 @@
+from collections import deque
+
 def solution(prices):
+    queue = deque(prices)
     answer = []
-    for i in range(len(prices)):
-        # print(i)
-        cnt=0
-        for j in range(i+1,len(prices)):
-            # print(j)
-            cnt+=1
-            if prices[i]>prices[j]:
-                break
-        answer.append(cnt)
+    
+    while queue:
+        price = queue.popleft()
+        print(price)
+        sec = 0
+        for q in queue:
+            print(q)
+            sec += 1
+            if price > q:
+                break 
+        answer.append(sec)        
     return answer
