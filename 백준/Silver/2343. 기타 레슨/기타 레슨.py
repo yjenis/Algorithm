e38c72c0121ce606a4b0a91462f1a02ecbee981d
YEJIN <147367459+yjenis@users.noreply.github.com>
2025-03-19 13:32:22 +0900

    [Silver I] Title: 기타 레슨, Time: 224 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -1,33 +1,34 @@
-def count_bluerays(lectures, size):
-    count = 1  # 블루레이 개수
-    total = 0  # 현재 블루레이에 담긴 강의 길이 합
+def divde(lectures,max_size,M): #조건에 맞는
+    cnt=1
+    current_sum=0
 
-    for lecture in lectures:
-        if total + lecture > size:  # 현재 블루레이에 더 담을 수 없는 경우
-            count += 1  # 새로운 블루레이 사용
-            total = lecture  # 새 블루레이에 강의 추가
+    for l in lectures:
+        if current_sum+l<=max_size:
+            current_sum+=l
         else:
-            total += lecture  # 현재 블루레이에 추가
-    
-    return count  # 사용한 블루레이 개수 반환
+            cnt+=1
+            current_sum=l
+            if cnt>M:
+                return False
+    return True
 
-def find_min_blueray_size(n, m, lectures):
-    left, right = max(lectures), sum(lectures)
-    answer = right
 
-    while left <= right:
-        mid = (left + right) // 2  # 블루레이 크기의 후보
-        if count_bluerays(lectures, mid) <= m:  # 블루레이 개수가 m 이하인지 확인
-            answer = mid  # 가능한 크기이므로 저장
-            right = mid - 1  # 더 작은 크기로 탐색
+def find_blu(N,M,lectures): #최적의 수 찾기
+    low=max(lectures)
+    high=sum(lectures)
+
+    while low<=high:
+        mid=int((low+high)/2)
+
+        if divde(lectures,mid,M): #되면 줄여
+            high=mid-1
         else:
-            left = mid + 1  # 크기를 키워야 함
+            low=mid+1 #안되면 늘
+    return low
 
-    return answer  # 가능한 블루레이 크기 중 최소값 반환
 
-# 입력
-n, m = map(int, input().split())
-lectures = list(map(int, input().split()))
+N,M=map(int,input().split())
+lectures=list(map(int,input().split()))
 
-# 결과 출력
-print(find_min_blueray_size(n, m, lectures))
+result=find_blu(N,M,lectures)
+print(result)
