fd1bdaca70cee846d367e88d53bffc78d30ae1f4
YEJIN <147367459+yjenis@users.noreply.github.com>
2025-03-21 00:22:52 +0900

    [Silver IV] Title: 주몽, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -0,0 +1,18 @@
+n=int(input())
+m=int(input())
+numbers=list(map(int,input().split()))
+numbers.sort()
+cnt=0
+start=0
+end=n-1
+
+while start<end:
+    if numbers[start]+numbers[end]==m:
+        cnt+=1
+        start+=1
+        end-=1
+    elif numbers[start]+numbers[end]<m:
+        start+=1
+    else:
+        end-=1
+print(cnt)
