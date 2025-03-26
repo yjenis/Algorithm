54caf687e5a7e15e8e7d4d87d53a6b2508b41147
YEJIN <147367459+yjenis@users.noreply.github.com>
2025-03-26 17:40:52 +0900

    [Silver IV] Title: 제로, Time: 2540 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -3,9 +3,9 @@ k=int(input())
 wallet=[]
 for i in range(k):
     a=int(input())
-    if a!=0:
-        wallet.append(a)
-    else:
+    if a==0:
         wallet.pop()
+    else:
+        wallet.append(a)
 
 print(sum(wallet))
